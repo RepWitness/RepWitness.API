21206b05a5339a116c606891db21dd35bff0fb62
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2026-03-04 14:24:45 +0200

    #REP-2 - add: CLEAN Arhitecture

---
+++ RepWitness.Application/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepWitness.Application
+{
+    internal class DependencyInjection
+    {
+    }
+}
---
+++ RepWitness.Persistence/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepWitness.Persistence
+{
+    internal class DependencyInjection
+    {
+    }
+}
