68c3a8281c9b5ef4a3c7dc43543697b1ef448756
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2026-03-04 16:13:08 +0200

    #REP-3 - add: database

---
+++ RepWitness.Persistence/Context/RepWitnessContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepWitness.Domain.Entities;
+
+namespace RepWitness.Persistence.Context;
+
+public class RepWitnessContext(DbContextOptions<RepWitnessContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Exercises> Exercises { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email).IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username).IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Role)
+            .WithMany()
+            .HasForeignKey(u => u.RoleId);
+
+        modelBuilder.Entity<Exercises>()
+            .HasOne(e => e.Category)
+            .WithMany()
+            .HasForeignKey(e => e.CategoryId);
+
+        modelBuilder.Entity<Exercises>()
+            .HasOne(e => e.User)
+            .WithMany()
+            .HasForeignKey(e => e.UserId);
+    }
+}
