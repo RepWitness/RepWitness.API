faef6cef595a5119a189bdf3634bd696c249423f
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2026-03-06 13:41:10 +0200

    #REP-5: fiix guid for id

--- RepWitness.Domain/Entities/Exercises.cs
+++ RepWitness.Domain/Entities/Exercises.cs
@@ -2,7 +2,7 @@
 
 public class Exercises
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public int CategoryId { get; set; }
--- RepWitness.Domain/Entities/User.cs
+++ RepWitness.Domain/Entities/User.cs
@@ -4,7 +4,7 @@ namespace RepWitness.Domain.Entities;
 
 public class User
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
